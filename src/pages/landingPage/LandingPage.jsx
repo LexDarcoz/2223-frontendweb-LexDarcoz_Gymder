28f1511fcfebb6456d1e95f0b2f87c7e86c0bd8b
LexDarcoz <Alexander.schatteman@gmail.com>
2022-12-15 20:52:44 +0100

    work mr render

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import About from "../../components/landingPage/About";
 import Carousel from "../../components/landingPage/Carousel";
 import Contact from "../../components/landingPage/Contact";
-
+// heih
 import Services from "../../components/landingPage/Services";
 import { useAuth0 } from "@auth0/auth0-react";
 import GymListTopThree from "../../components/gymTools/GymListTopThree";
